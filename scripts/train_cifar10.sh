29a5f9e7d02b9a642a39824205d7a0584d9e763d
DONGHWA KANG <oktaylor19@gmail.com>
2026-06-22 17:54:42 -0700

    edit slurm script

--- scripts/train_cifar10.sh
+++ scripts/train_cifar10.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --account=def-awolson
-#SBATCH --time=08:00:00
+#SBATCH --time=06:30:00
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:nvidia_h100_80gb_hbm3_2g.20gb:1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=32000M
-#SBATCH --job-name=cifar10
+#SBATCH --mem=16000M
+#SBATCH --job-name=lr0030_1000ep_bs512
 #SBATCH --output=/scratch/%u/adaptovision-replication/outputs/slurm/cifar10_%j.out
 #SBATCH --error=/scratch/%u/adaptovision-replication/outputs/slurm/cifar10_%j.err
 
@@ -22,5 +22,13 @@ mkdir -p outputs/runs
 
 source .venv/bin/activate
 
+CONFIG_PATH=configs/lr00125_1000ep_bs512.yaml
+
+echo "Using config: $CONFIG_PATH"
+
+
 python -m adaptovision.modeling.train \
-  --config configs/cifar10.yaml
+  --config "$CONFIG_PATH"
+
+python -m adaptovision.modeling.evaluate \
+  --config "$CONFIG_PATH"
